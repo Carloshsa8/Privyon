15c7caaf5350207614401dae7a0d02827da7e54a
Carloshsa8 <carloshsa8@hotmail.com>
2026-05-20 23:56:30 -0300

    Att TaFeito

---
+++ Apps/TaFeito./assets/index-DgxFiJ_-.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Cn3-XPg3.js","assets/index-JaBZAU3q.js"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-JaBZAU3q.js";const _=p("App",{web:()=>r(()=>import("./web-Cn3-XPg3.js"),__vite__mapDeps([0,1])).then(e=>new e.AppWeb)});export{_ as App};
